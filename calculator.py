fc73dd3b259a5f9a4b88a7c9a5bb260382727634
hamzabaig0 <h.baig@ufl.edu>
2025-04-13 21:37:10 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -16,40 +16,33 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+# calculator.py
+# Defines functions used to create a simple calculator
+# One function per operation, in order.
+
 import math
 
-
 def square_root(a):
-    try:
-        return math.sqrt(a)
-    except ValueError as e:
-        print(f"Cannot take square root of a negative number.")
-
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number.")
+    return math.sqrt(a)
 
 def hypotenuse(a, b):
-    try:
-        return math.hypot(a, b)  # can have negative nums
-    except ValueError as e:
-        print(f"Neither sides can equal 0")
-
+    return math.hypot(a, b)  # Accepts 0 or negative values, no error needed
 
 def add(a, b):
     return a + b
 
-
 def subtract(a, b):
     return a - b
 
-
 def multiply(a, b):
     return a * b
-
 
 def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
-
 
 def logarithm(a, b):
     if a <= 0 or a == 1:
