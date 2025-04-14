231c6b01147b9a007d9f51a35ed08901ed6cc970
hamzabaig0 <h.baig@ufl.edu>
2025-04-13 20:58:43 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,16 +9,47 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+"""
+calculator.py
+- Defines functions used to create a simple calculator
+
+One function per operation, in order.
+"""
+# First example
 import math
 
+
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError as e:
+        print(f"Cannot take square root of a negative number.")
+
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)  # can have negative nums
+    except ValueError as e:
+        print(f"Neither sides can equal 0")
+
+
 def add(a, b):
-    return a+b
+    return a + b
+
 
 def subtract(a, b):
-    return a-b
+    return a - b
+
 
 def multiply(a, b):
-    return a*b
+    return a * b
+
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
 
 def logarithm(a, b):
     if a <= 0 or a == 1:
@@ -26,27 +57,3 @@ def logarithm(a, b):
     if b <= 0:
         raise ValueError("Argument 'b' must be positive.")
     return math.log(b, a)
-
-def exponent(a, b):
-    return a**b
-
-def sub(a, b):
-    return a-b
-
-def mul(a, b):
-    return a*b
-
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return b / a
-
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Base 'a' must be positive and not equal to 1.")
-    if b <= 0:
-        raise ValueError("Argument 'b' must be positive.")
-    return math.log(b, a)
-
-def exp(a, b):
-    return a**b
